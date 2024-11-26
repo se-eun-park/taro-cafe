1452fec6052fc1064e3122fc1756cecbe3eb8599
kwakseongjae <gkffhdnls13@gmail.com>
2024-11-27 03:19:39 +0900

    feat: 높이 dvh로 변경

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function index() {
   return (
-    <div className='flex items-center justify-center w-full h-screen bg-black font-uuun'>
+    <div className='flex items-center justify-center w-full bg-black h-dvh font-uuun'>
       <Outlet />
     </div>
   )
