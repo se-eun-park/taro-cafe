38ce88d8315598c0a9eda5a99b197ffb9a643e32
박세은 <seeunbag30@gmail.com>
2024-12-01 21:38:53 +0900

    feat: QR 체크 페이지 이동

--- src/routes/pages/Money.tsx
+++ src/routes/pages/Money.tsx
@@ -1,4 +1,3 @@
-import { QRCodeSVG } from 'qrcode.react'
 import { encodeBase64 } from '@/hooks/useAes'
 import { MoneyContentsMap } from '@/constants/MoneyContentsMap'
 import { HomeButton } from '@assets/svg'
@@ -9,20 +8,20 @@ export default function Money() {
 
   const encodedValue = encodeBase64(`money/${randomValue}`)
 
-  const downloadUrl = `https://flip-side-virid.vercel.app/download/${encodedValue}`
-
   return (
     <div className='flex gap-[60px] items-center'>
       <MoneyContent.component className='w-[300px] h-[300px]' />
       <div className='flex flex-col w-[450px] gap-10'>
         <div className='flex flex-col gap-8 text-white'>
-          <div className='flex items-center justify-between'>
-            <h1 className='text-[28px]'>{MoneyContent.title}</h1>
-            <QRCodeSVG value={downloadUrl} className='w-20 h-fit' />
-          </div>
+          <h1 className='text-[28px]'>{MoneyContent.title}</h1>
           <p className='text-xl'>{MoneyContent.description}</p>
         </div>
-        <HomeButton className='w-[450px] h-[72px]' text='PRINT' textSize='md' navigate='/print' />
+        <HomeButton
+          className='w-[450px] h-[72px]'
+          text='PRINT'
+          textSize='md'
+          navigate={`/print/${encodedValue}`}
+        />
       </div>
     </div>
   )
