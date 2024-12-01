8521537255d813a9ba4ecbf1ba30b468d64de75a
kwakseongjae <gkffhdnls13@gmail.com>
2024-12-01 21:18:22 +0900

    design: layout 상단 패딩 78px 추가

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function index() {
   return (
-    <div className='flex items-center justify-center w-full bg-black h-dvh font-uuun'>
+    <div className='flex items-center justify-center w-full bg-black h-dvh font-uuun pt-[78px]'>
       <Outlet />
     </div>
   )
