e061da079ea53e29f7523879c047d5a84c3fdec3
kwakseongjae <gkffhdnls13@gmail.com>
2024-12-01 21:22:35 +0900

    feat: 로딩 비디오 이미지 사이즈 조정

--- src/routes/pages/Loading.tsx
+++ src/routes/pages/Loading.tsx
@@ -10,19 +10,15 @@ const Loading = () => {
 
   return (
     <div className='flex flex-col gap-[70px] items-center relative'>
-      <h1 className='absolute top-10 text-[40px] text-white'>지금부터 커피 리딩을 시작할게요.</h1>
       <video
         autoPlay
         muted
         playsInline
         onEnded={handleVideoEnd}
-        style={{ width: '700px', height: 'auto' }}
+        style={{ width: '1112px', height: '834px' }}
       >
         <source src={`/redirect-video.mp4`} type='video/mp4' />
       </video>
-      <h1 className='absolute bottom-10 text-[32px] text-[#6D6C69] pt-3'>
-        We'll start the coffee reading now.
-      </h1>
     </div>
   )
 }
